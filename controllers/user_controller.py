8579d6571d0ebb79d3b00c2cfcdcc3240a3f2879
a.atalla <a.3talla@gmail.com>
2017-11-01 01:06:23 +0200

    add missing file list_book.py

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -6,11 +6,9 @@ s = Session()
 class UserController(object):
 
     def get_all(self):
-        
         all_users = s.query(User).all()
-        
         return all_users
-        
+
     def add_user(self, username, password, first_name, last_name):
         u = User(username, password, first_name, last_name)
         s.add(u)
--- ui/home_page.py
+++ ui/home_page.py
@@ -1,6 +1,7 @@
 from ui.wxform.gui import PnlHome
 from controllers import UserController
 
+
 class HomePage(PnlHome):
     def __init__(self, parent):
         super(HomePage, self).__init__(parent)
@@ -10,21 +11,21 @@ class HomePage(PnlHome):
         self.users_view.AppendTextColumn("Last Name")
         self.users_view.AppendTextColumn("username")
         self.users_view.AppendTextColumn("Password")
-        
+
         # Get data and populate in teh view
         self.populate_user_view()
-        
+
     def populate_user_view(self):
-        
+
         uc = UserController()
-        users = uc.get_all() 
-        
+        users = uc.get_all()
+
         for user in users:
             self.users_view.AppendItem([str(user.id),
                                         user.first_name,
                                         user.last_name,
                                         user.username,
                                         user.password])
-        
+
     def on_btn_test(self, event):
         print('Test Button Clicked')
